4150823323bfc5ae5b5c747ee6e4e8a557994c97
thavoo <gustavoh.2312@gmail.com>
2018-07-10 05:42:42 -0400

    Add column to table users

---
+++ database/migrations/2018_07_10_093834_add_profession_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProfessionToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            // agregando una columna de la tabla users  after es para colocar el campo luego de password
+            $table->string('profession', 50)->nullable()->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('profession');
+        });
+    }
+}
